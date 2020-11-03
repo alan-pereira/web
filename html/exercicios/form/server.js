3c8e18ad93e497b052c7301dbbd2b314972a8529
Alan Pereira <alan.pereira@petragold.com.br>
2020-11-03 16:14:19 -0300

    html

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (req, res) =>{
   console.log(req.body)
-  res.send('<h1>Parabéns</h1>')
+  res.send('<h1>Parabéns. Usuário incluido!</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) =>{
+  console.log(req.params.id)
+  console.log(req.body)
+  res.send('<h1>Parabéns. Usuário alterado!</h1>')
 })
 
 
